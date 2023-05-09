613540783f9ad10a4e679ee5eeda3eec77670ba9
Abdirazak140 <abdirazak140@gmail.com>
2023-05-08 23:31:53 -0400

    Users can now make predictions and save them

--- src/components/predictionBox.tsx
+++ src/components/predictionBox.tsx
@@ -1,6 +1,6 @@
 function Prediction(props: any){
     return(
-        <div className="w-60 h-36 text-center text-black bg-white border-black rounded-md">{props.bracket}</div>
+        <div className="flex justify-center items-center w-60 h-36 text-lg text-black bg-white border-black rounded-lg m-3 border-2">{props.bracket}</div>
     )
 }
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,22 +6,48 @@ import { getAuth } from "firebase/auth";
 import Prediction from "../components/predictionBox";
 import ReactDOM from "react-dom";
 import { useEffect } from "react";
+import axios from "axios";
 
 function Dashboard(){
     const auth = getAuth(fireApp);
     const userId = auth.currentUser?.uid;
 
     function createPrediction(event: any){
+        event.preventDefault();
         const selectedLeague = document.querySelector("select[name='sportsleague']") as HTMLSelectElement;
         const league: string = selectedLeague.value;
         const container = document.getElementById("dashboard");
 
+        retrieveLeagueData(league);
+
         set(ref(database, `users/${userId}/predictions`),{
-            [league]: {}
+            [league]: null
         })
+
         const newPrediction = document.createElement("div");
         ReactDOM.render(<Prediction bracket={league}/>, newPrediction)
         container?.appendChild(newPrediction);
+    }
+
+    async function retrieveLeagueData(league: string){
+        // if (league === ""){
+
+        // }
+        const options = {
+            method: 'GET',
+            url: 'https://basketapi1.p.rapidapi.com/api/basketball/group/tournament/4618/season/22873/matches',
+            headers: {
+              'X-RapidAPI-Key': 'f25a3c0601msh66d2f137054c227p1a9429jsn9b16330dc6c6',
+              'X-RapidAPI-Host': 'basketapi1.p.rapidapi.com'
+            }
+          };
+        
+        try {
+            const response = await axios(options);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     useEffect(() => {
@@ -39,15 +65,15 @@ function Dashboard(){
                         <h1>Select a Sports League:</h1>
                         <select name="sportsleague" className="mt-4">
                             <option value="NBA Playoffs">Nba Playoffs</option>
-                            <option value="NFL Playoffs">NFL Playoffs</option>
-                            <option value="Stanley Cup Playoffs">Stanley Cup Playoffs</option>
+                            {/* <option value="NFL Playoffs">NFL Playoffs</option>
+                            <option value="Stanley Cup Playoffs">Stanley Cup Playoffs</option> */}
                         </select>
                         <button onClick={createPrediction} type="submit" className="rounded-full bg-zinc-300 shadow-sm w-36 h-11 mt-11">Create</button>
                     </form>
                 </Popup>
                 <button className="dashboard-btn text-red-500 border-red-500 border-2">Delete Prediction</button>
             </div>
-            <div id="dashboard" className="w-full h-screen bg-slate-200 flex flex-wrap"></div>
+            <div id="dashboard" className="w-full h-screen bg-slate-200 flex flex-wrap pt-40"></div>
         </div>
     )
 }
