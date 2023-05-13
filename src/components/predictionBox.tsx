20dfe1145aaf75d66268e8edafece64462eb35d6
Abdirazak140 <abdirazak140@gmail.com>
2023-05-12 23:10:22 -0400

    Made prediction builder panel

--- src/components/predictionBox.tsx
+++ src/components/predictionBox.tsx
@@ -1,6 +1,6 @@
-function Prediction(props: any){
+function Prediction({bracket, sendUser}: any){
     return(
-        <div className="flex justify-center items-center w-60 h-36 text-lg text-black bg-white border-black rounded-lg m-3 border-2">{props.bracket}</div>
+        <div onClick={sendUser} className="flex justify-center items-center w-60 h-36 text-lg text-black bg-white border-black rounded-lg m-3 border-2">{bracket}</div>
     )
 }
 
