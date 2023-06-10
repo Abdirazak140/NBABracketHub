fc08c904f487ad7b86d2d50055b1e8a743b8ea20
Abdirazak140 <abdirazak140@gmail.com>
2023-06-09 22:34:29 -0400

    Update MakePredictions.tsx

--- src/pages/MakePredictions.tsx
+++ src/pages/MakePredictions.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../App";
-import { child, get, ref } from "firebase/database";
+import { child, get, ref, set } from "firebase/database";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import cheerio from "cheerio";
@@ -25,6 +25,136 @@ function MakePredictions(){
         getTeams();
         RetreiveSemiFinalsData();
     },[userId, league])
+
+    function retrieveLeagueData(league: string){
+        const espnUrl = 'http://www.espn.com/nba/bracket';
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const newUrl = proxyUrl + espnUrl;
+        axios.get(newUrl).then(response => {
+            let teams: Array<string> = [];
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            //first round
+            const firstRound = $(".c1");
+            firstRound.each((_: any, dl: any) => {
+                $(dl).find("dt").each((_: any , dt: any) => {
+                    let text = $(dt).text().trim();
+                    const textArr = text.split("(");
+                    const team1_text = textArr[1].split(")");
+                    const team2_text = textArr[2].split(")");
+                    const team1 = team1_text[1].substring(1);
+                    const team2 = team2_text[1].substring(1);
+                    teams.push(team1);
+                    teams.push(team2);
+                })
+            })
+            let counter: number = 0;
+            for (let i: number = 1; i <= 8; i++){
+                set(ref(database, `users/${userId}/predictions/${league}/r1/match_${i}`),{
+                    team_1: teams[counter],
+                    team_2: teams[counter+1],
+                })
+                counter+=2;
+            }
+
+            //second round
+            teams = [];
+            const secondRound = $(".c2");
+            secondRound.each((_: any, dl: any) => {
+                $(dl).find("dt").each((_: any , dt: any) => {
+                    let numOfTeams = $(dt).find("a");
+                    if (numOfTeams.length === 1){
+                        let text = $(numOfTeams).text().trim();
+                        teams.push(text);
+                        teams.push("TBA");
+                    }
+                    else if (numOfTeams.length === 0){
+                        teams.push("TBA", "TBA");
+                    }
+                    else{
+                        $(dt).find("a").each((_: any, a: any) => {
+                            let text = $(a).text().trim();
+                            teams.push(text);
+                        })
+                    }
+                })
+            })
+            counter = 0;
+            for (let i: number = 1; i <= 4; i++){
+                set(ref(database, `users/${userId}/predictions/${league}/r2/match_${i}`),{
+                    team_1: teams[counter],
+                    team_2: teams[counter+1],
+                })
+                counter+=2;
+            }
+
+
+            //third round
+            teams = [];
+            const thirdRound = $(".c3");
+            thirdRound.each((_: any, dl: any) => {
+                $(dl).find("dt").each((_: any , dt: any) => {
+                    let numOfTeams = $(dt).find("a");
+                    if (numOfTeams.length === 1){
+                        let text = $(numOfTeams).text().trim();
+                        teams.push(text);
+                        teams.push("TBA");
+                    }
+                    else if (numOfTeams.length === 0){
+                        teams.push("TBA", "TBA");
+                    }
+                    else{
+                        $(dt).find("a").each((_: any, a: any) => {
+                            let text = $(a).text().trim();
+                            teams.push(text);
+                        })
+                    }
+                })
+            })
+            counter = 0;
+            for (let i: number = 1; i <= 2; i++){
+                set(ref(database, `users/${userId}/predictions/${league}/r3/match_${i}`),{
+                    team_1: teams[counter],
+                    team_2: teams[counter+1],
+                })
+                counter+=2;
+            }
+
+            
+            //final round
+            teams = [];
+            const finalRound = $(".c4");
+            finalRound.each((_: any, dl: any) => {
+                $(dl).find("dt").each((_: any , dt: any) => {
+                    let numOfTeams = $(dt).find("a");
+                    if (numOfTeams.length === 1){
+                        let text = $(numOfTeams).text().trim();
+                        teams.push(text);
+                        teams.push("TBA");
+                    }
+                    else if (numOfTeams.length === 0){
+                        teams.push("TBA", "TBA");
+                    }
+                    else{
+                        $(dt).find("a").each((_: any, a: any) => {
+                            let text = $(a).text().trim();
+                            teams.push(text);
+                        })
+                    }
+                })
+            })
+            counter = 0;
+            for (let i: number = 1; i <= 1; i++){
+                set(ref(database, `users/${userId}/predictions/${league}/r4/match_${i}`),{
+                    team_1: teams[counter],
+                    team_2: teams[counter+1],
+                })
+                counter+=2;
+            }
+        })
+    }
+    
 
     function RetreiveSemiFinalsData(){
         const espnUrl = 'http://www.espn.com/nba/bracket';
