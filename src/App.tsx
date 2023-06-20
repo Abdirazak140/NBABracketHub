e730408015497ca12889c800be518ea6e336231e
Abdirazak140 <abdirazak140@gmail.com>
2023-06-20 11:58:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ import Overview from "./pages/Overview";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCU-njROKe-uM_yc03aUabAmAuomAP8Cv4",
-  authDomain: "sportbracketpredictions.firebaseapp.com",
-  databaseURL: "https://sportbracketpredictions-default-rtdb.firebaseio.com/",
-  projectId: "sportbracketpredictions",
-  storageBucket: "sportbracketpredictions.appspot.com",
-  messagingSenderId: "826690539160",
-  appId: "1:826690539160:web:91d50a78a009d6f7df8aed",
-  measurementId: "G-DLSPKFYJVD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const fireApp = initializeApp(firebaseConfig);
